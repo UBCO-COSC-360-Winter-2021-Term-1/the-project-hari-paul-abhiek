f8ff63c5c5273c68e131695e80419052565112ab
Paul <psciarpe@gmail.com>
2021-11-03 14:42:29 -0700

    paul add- format columns, carousel img change N/W

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,6 @@
-var myCarousel = document.querySelector('#myCarousel');
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-});
+// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+// var myCarousel = document.querySelector('#myCarousel')
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//   interval: 2000,
+//   wrap: false
+// })
