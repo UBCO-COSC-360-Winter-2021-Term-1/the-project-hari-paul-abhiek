f14ba17ade5c1ea29bb21433a0511b0272a57ca9
Paul <psciarpe@gmail.com>
2021-11-08 12:47:45 -0800

    carousel is working now!

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,6 @@
-// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
-// var myCarousel = document.querySelector('#myCarousel')
-// var carousel = new bootstrap.Carousel(myCarousel, {
-//   interval: 2000,
-//   wrap: false
-// })
+window.bootstrap = require('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js');
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
