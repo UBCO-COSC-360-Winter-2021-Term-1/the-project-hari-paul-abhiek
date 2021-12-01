9de6f9da61c782e2c59d93556f094470dfd70b11
Paul <61891921+shecallmedata@users.noreply.github.com>
2021-11-30 16:31:38 -0800

    Add files via upload
    database

---
+++ userdata.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 09, 2017 at 08:05 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_sample`
+--
+
+-- --------------------------------------------------------
+
+
+
+
+--
+-- Table structure for table `admin`
+
+
+-- --------------------------------------------------------
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+
+INSERT INTO `admin` (`username`, `pass`) VALUES
+--password s
+('paul', '03c7c0ace395d80182db07ae2c30f034'),
+--password t
+('hari', 'e358efa489f58062f10dd7316b65649e'),
+--password y
+('bist', '415290769594460e2e485922904f345d');
+-- --------------------------------------------------------
+
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `cid` int(11) NOT NULL,
+  `body` varchar(400) NOT NULL,
+  `postid` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `commentdate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `pic` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `firstName`, `lastName`, `email`, `password`) VALUES
+('dvader', 'darth', 'vader', 'vader@dark.force', '0f359740bd1cda994f8b55330c86d845');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Table structure for table `trails post`
+--
+
+CREATE TABLE `trails` (
+  `trailId` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userImages`
+--
+
+CREATE TABLE `userImages` (
+  `userID` int(11) NOT NULL,
+  `contentType` varchar(255) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+
+
+--
+-- Indexes for table `userImages`
+--
+ALTER TABLE `userImages`
+  ADD PRIMARY KEY (`userID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `userImages`
+--
+ALTER TABLE `userImages`
+  ADD CONSTRAINT `userimages_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
