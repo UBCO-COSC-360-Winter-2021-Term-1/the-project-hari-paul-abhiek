d784dfb785a09577637ffbba8bc29bbdd80877fc
dudeImpossible42 <hvidimlic1234@hotmail.com>
2021-12-06 13:35:43 -0800

    chenge i to I

--- src/server/logout.php
+++ src/server/logout.php
@@ -2,8 +2,8 @@
 <html>
 <?php
 session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    unset($_SESSION['loggedin']);
+if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
+    unset($_SESSION['loggedIn']);
     header('Location: ./../client/index.php');
 } else {
     header('Location: ./../client/'.$_SERVER['HTTP_REFERER'].'');
