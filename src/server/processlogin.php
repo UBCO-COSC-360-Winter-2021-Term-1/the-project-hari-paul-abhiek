f5b1e97c1b525bb263ab7d2317fedc0f253c2cfe
Bist07 <74443096+Bist07@users.noreply.github.com>
2021-12-06 03:32:15 -0800

    Update processlogin.php

--- src/server/processlogin.php
+++ src/server/processlogin.php
@@ -9,50 +9,28 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true)
     header('Location: ./../client/index.php');
 else{
 
-    function generateRandomSalt() {
-        return base64_encode(random_bytes(12))  ;
+    $password = md5($password);
+
+    $sql = "SELECT username, password FROM users";
+    $results = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_assoc($results)) {
+        $uname = $row['username'];
+        $pass = $row['password'];
+
+        if($uname == $username && $password == $pass){
+            
+            echo "Login Succesfull!";
+            $_SESSION['loggedin'] = true;
+            header('Location: ./../client/index.php');
+            break;
+        }else
+        header('Location: ./../server/login.php');
+        echo "alert(\"wrong username or password entered\")";
+    } 
+
+mysqli_free_result($results);
+mysqli_close($conn);
     }
-
-    function validateUser ($username, $password) {
-        $sql = "SELECT password FROM users WHERE username='$username'";
-        $result = mysqli_query($link, $sql); //execute the query
-        if($row = mysqli_fetch_assoc($result)){
-        
-        //username exists, build second query with salt
-        $salt = $row['Salt'];
-        $saltSql = "SELECT userID FROM users WHERE username='$username'AND Password=MD5('$password$salt')";
-        
-        $finalResult = mysqli_query($link, $saltSql1);
-
-        if($finalrow = mysqli_fetch_assoc($finalResult)){
-        return true; 
-        }
-
-        return false; 
-        
-        }
-			$sql = "SELECT username, password FROM users";
-            $results = mysqli_query($conn, $sql);
-
-			while ($row = mysqli_fetch_assoc($results)) {
-				$uname = $row['username'];
-				$pass = $row['password'];
-
-				if($uname == $username && $password == $pass){
-                    
-					echo "Login Succesfull!";
-                    $_SESSION['loggedIn'] = true;
-                    header('Location: ./../client/index.php');
-					break;
-				}else
-                $no_match_error = "Wrong username or password!";
-                echo "<script type='text/javascript'>alert('$no_match_error');</script>";
-			} 
-
-    mysqli_free_result($results);
-    mysqli_close($conn);
-    }
-    header('Location: ./../client/index.php');
-}
 ?>
 </html>
